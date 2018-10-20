b610cdb0efce00892cf87c8cfd163b0de284bdf7
Andrea Peretti <peretti.and@gmail.com>
2018-10-20 19:08:40 +0200

    Added a simple object following camera script

---
+++ Assets/Scripts/Camera/FollowingCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace EndlessRun.Camera
+{
+   public class FollowingCamera : MonoBehaviour
+   {
+      public GameObject objectToFollow;
+
+      /////////////////////////////////////////////
+      void Start()
+      {
+#if DEBUG //Let's assume that when the release is built, theese checks are passed
+         if (objectToFollow == null)
+         {
+            Debug.LogError("CameraManager " + name + ": component not correctly initialized.");
+            enabled = false;
+            return;
+         }
+#endif
+      }
+
+      /////////////////////////////////////////////
+      void Update()
+      {
+         Vector3 position = transform.position;
+         position.z = objectToFollow.transform.position.z;
+         transform.position = position;
+      }
+   }
+}
